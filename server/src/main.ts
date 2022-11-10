c4f656b71198f19c46e9882638974b892bc7ce02
KIMBEOBWOO <qjqdn1568@naver.com>
2022-11-10 18:09:11 +0900

    refactor >>> app module
    1. remove app.service, app.controller
    2. main.ts using env SERVER_PORT added

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT || 3000);
 
   Logger.log(
     `Nest.js is running on Port [${process.env.SERVER_PORT}], using ENV mode [${process.env.NODE_ENV}]`,
