5f2b81b184af840e4521b4b05f42547333beeea8
KIMBEOBWOO <qjqdn1568@naver.com>
2022-11-21 22:42:44 +0900

    feat >>> configuration module - cache config added

--- server/src/modules/config/configuration.ts
+++ server/src/modules/config/configuration.ts
@@ -1,14 +1,18 @@
 import * as Joi from 'joi';
+import { IsCacheConfig } from './interfaces/cache.config.interface';
 import { IsPostgresDatabaseConfig } from './interfaces/postgres-database.config.interface';
 
-export default (): IsPostgresDatabaseConfig => {
+export default () => {
   // joi validation target schema
-  const schema = Joi.object<IsPostgresDatabaseConfig, true>({
+  const schema = Joi.object<IsPostgresDatabaseConfig & IsCacheConfig, true>({
     postgresHost: Joi.string().required(),
     postgresPort: Joi.number().required(),
     postgresUserName: Joi.string().required(),
     postgresPassword: Joi.string().required(),
     postgresDatabaseName: Joi.string().required(),
+
+    cacheHost: Joi.string().required(),
+    cachePort: Joi.number().required(),
   });
 
   // validate target values
@@ -18,12 +22,15 @@ export default (): IsPostgresDatabaseConfig => {
     postgresUserName: process.env.POSTGRES_USERNAME,
     postgresPassword: process.env.POSTGRES_PASSWORD,
     postgresDatabaseName: process.env.POSTGRES_DATABASE,
+
+    cacheHost: process.env.CACHE_HOST,
+    cachePort: process.env.CACHE_PORT,
   };
 
   const { error, value } = schema.validate(config);
 
   if (error) {
-    throw new Error('Env Missing ' + error.details.toString());
+    throw new Error('Env Missing ' + error.message);
   }
 
   return value;
---
+++ server/src/modules/config/interfaces/cache.config.interface.ts
@@ -0,0 +1,4 @@
+export interface IsCacheConfig {
+  cacheHost: string;
+  cachePort: number;
+}
