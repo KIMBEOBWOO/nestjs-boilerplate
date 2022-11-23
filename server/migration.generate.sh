8403a80ced3ad5a34303ec082866b514b008c33d
KIMBEOBWOO <qjqdn1568@naver.com>
2022-11-23 23:44:43 +0900

    fix >>> migration script using env path

--- server/migration.generate.sh
+++ server/migration.generate.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-# The first parameter provides the path to the data source file
+# The first parameter should provide an environment to which migration will be applied.
+# Select dev, production.
+Environment=$1
+
+# The second parameter provides the path to the data source file
 # If the path does not need to be received when executing the script file,
 # substitute a string to the value of the corresponding variable.
-DataSourcePath=$1
-# The second parameter provides the path through which the migration file will be created
-GeneratedMigrationFilePath=$2
+DataSourcePath=$2
+# The third parameter provides the path through which the migration file will be created
+GeneratedMigrationFilePath=$3
 
 # Generate migration, if No changes in database schema were found, Error code 1
-yarn run typeorm --dataSource=${DataSourcePath} migration:generate ${GeneratedMigrationFilePath}
+yarn run typeorm:${Environment} --dataSource=${DataSourcePath} migration:generate ${GeneratedMigrationFilePath}
--- server/migration.run.sh
+++ server/migration.run.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-# The first parameter provides the path to the data source file
+# The first parameter should provide an environment to which migration will be applied.
+# Select dev, production.
+Environment=$1
+
+# The Second parameter provides the path to the data source file
 # If the path does not need to be received when executing the script file,
 # substitute a string to the value of the corresponding variable.
-DataSourcePath=$1
+DataSourcePath=$2
 
-yarn run typeorm --dataSource=${DataSourcePath} migration:run
+yarn run typeorm:${Environment} --dataSource=${DataSourcePath} migration:run
