002da995655d60df3dd208ee6b09e94184928d25
KIMBEOBWOO <qjqdn1568@naver.com>
2022-11-26 15:50:47 +0900

    feat >>> added IsRedisConfig

---
+++ server/src/modules/config/interfaces/redis.config.ts
@@ -0,0 +1,8 @@
+/**
+ * @name REDIS_CONFIG
+ * - this is interface, redis (in memory cache) config environment variable
+ */
+export interface IsRedisConfig {
+  readonly redisHost: string; // redis config host
+  readonly redisPort: number; // redis config port (default 6379)
+}
