4e8a1920c6609673773fcd79422a3feec2971428
KIMBEOBWOO <qjqdn1568@naver.com>
2022-11-26 16:10:53 +0900

    feat >>> redis cache module init

---
+++ server/src/modules/cache/cache.module.ts
@@ -0,0 +1,25 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-store';
+import { IsRedisConfig } from '../config/interfaces/redis.config';
+import { REDIS_TOKEN } from './symbols/redis.symbol';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: REDIS_TOKEN,
+      useFactory: async (configService: ConfigService<IsRedisConfig>) =>
+        await redisStore({
+          socket: {
+            host: configService.get('redisHost'),
+            port: configService.get('redisPort'),
+          },
+          ttl: 10,
+        }),
+      inject: [ConfigService<IsRedisConfig>],
+    },
+  ],
+  exports: [REDIS_TOKEN],
+})
+export class ServerCacheModule {}
