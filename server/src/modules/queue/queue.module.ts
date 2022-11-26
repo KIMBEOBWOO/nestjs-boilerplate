0dae4b2569d0a03fe206410275c18d1ead2262ab
KIMBEOBWOO <qjqdn1568@naver.com>
2022-11-26 19:05:11 +0900

    feat >>> bull module with redis!

---
+++ server/src/modules/queue/queue.module.ts
@@ -0,0 +1,33 @@
+import { BullModule } from '@nestjs/bull';
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { IsRedisConfig } from '../config/interfaces/redis.config';
+
+@Module({
+  imports: [
+    // import Bull Module
+    BullModule.forRootAsync({
+      useFactory: async (
+        configService: ConfigService<IsRedisConfig, true>,
+      ) => ({
+        redis: {
+          /**
+           * @link https://docs.nestjs.com/techniques/queues#producers
+           * if you needs more customized options, click link
+           *
+           * limiter: RateLimiter - Options to control the rate at which the queue's jobs are processed. See RateLimiter for more information. Optional.
+           * redis: RedisOpts - Options to configure the Redis connection. See RedisOpts for more information. Optional.
+           * prefix: string - Prefix for all queue keys. Optional.
+           * defaultJobOptions: JobOpts - Options to control the default settings for new jobs. See JobOpts for more information. Optional.
+           * settings: AdvancedSettings - Advanced Queue configuration settings. These should usually not be changed. See AdvancedSettings for more information. Optional.
+           */
+          host: configService.get('redisHost'),
+          port: configService.get('redisPort'),
+          password: configService.get('redisPassword'),
+        },
+      }),
+      inject: [ConfigService<IsRedisConfig, true>],
+    }),
+  ],
+})
+export class QueueModule {}
