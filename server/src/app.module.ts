b555228b6dff34315638ee6038c50aeb19d96ca4
KIMBEOBWOO <qjqdn1568@naver.com>
2023-07-21 17:08:16 +0900

    ✅ Transactional Module 적용

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,22 @@ import { ServerCacheModule } from './cache/cache.module';
 import { ServerConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { QueueModule } from './queue/queue.module';
+import { AopModule } from '@toss/nestjs-aop';
+import { TransactionModule } from 'typeorm-aop-transaction';
+import { POSTGRES_CONNECTION } from './database/const/postgres-connection.const';
 
 @Module({
-  imports: [ServerConfigModule, ServerCacheModule, QueueModule, DatabaseModule],
+  imports: [
+    AopModule,
+    TransactionModule.regist({
+      defaultConnectionName: POSTGRES_CONNECTION,
+      logging: 'all',
+    }),
+    ServerConfigModule,
+    ServerCacheModule,
+    QueueModule,
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
